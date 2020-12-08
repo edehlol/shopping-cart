fec60f4c999abbb17aa433ff6d351825b61b26a8
edehlol <edymusajev@gmail.com>
2020-12-08 23:03:56 +0200

    initial config

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import NavigationBar from './NavigationBar';
+
+const App = () => {
+  return (
+    <div>
+      <NavigationBar />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/components/NavigationBar.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const NavigationBar = () => {
+  return (
+    <div className="h-16 bg-indigo-50">
+      <div>nav</div>
+    </div>
+  );
+};
+
+export default NavigationBar;
